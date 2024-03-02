6fedea0097035116d16306a326ae26bd386d51b5
ymkk0811 <ymkk.hana@docomo.ne.jp>
2024-03-02 21:25:30 +0900

    従業員更新画面課題提出

---
+++ src/main/java/com/techacademy/controller/EmployeeController.java
@@ -0,0 +1,178 @@
+package com.techacademy.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.techacademy.constants.ErrorKinds;
+import com.techacademy.constants.ErrorMessage;
+
+import com.techacademy.entity.Employee;
+import com.techacademy.service.EmployeeService;
+import com.techacademy.service.UserDetail;
+
+@Controller
+@RequestMapping("employees")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    // 従業員一覧画面
+    @GetMapping
+    public String list(Model model) {
+
+        model.addAttribute("listSize", employeeService.findAll().size());
+        model.addAttribute("employeeList", employeeService.findAll());
+
+        return "employees/list";
+    }
+
+    // 従業員詳細画面
+    @GetMapping(value = "/{code}/")
+    public String detail(@PathVariable String code, Model model) {
+
+        model.addAttribute("employee", employeeService.findByCode(code));
+        return "employees/detail";
+    }
+
+    // 従業員新規登録画面
+    @GetMapping(value = "/add")
+    public String create(@ModelAttribute Employee employee) {
+
+        return "employees/new";
+    }
+
+    // 従業員新規登録処理
+    @PostMapping(value = "/add")
+    public String add(@Validated Employee employee, BindingResult res, Model model) {
+
+        // パスワード空白チェック
+        /*
+         * エンティティ側の入力チェックでも実装は行えるが、更新の方でパスワードが空白でもチェックエラーを出さずに
+         * 更新出来る仕様となっているため上記を考慮した場合に別でエラーメッセージを出す方法が簡単だと判断
+         */
+        if ("".equals(employee.getPassword())) {
+            // パスワードが空白だった場合
+            model.addAttribute(ErrorMessage.getErrorName(ErrorKinds.BLANK_ERROR),
+                    ErrorMessage.getErrorValue(ErrorKinds.BLANK_ERROR));
+
+            return create(employee);
+
+        }
+
+        // 入力チェック
+        if (res.hasErrors()) {
+            return create(employee);
+        }
+
+        // 論理削除を行った従業員番号を指定すると例外となるためtry~catchで対応
+        // (findByIdでは削除フラグがTRUEのデータが取得出来ないため)
+        try {
+            ErrorKinds result = employeeService.save(employee);
+
+            if (ErrorMessage.contains(result)) {
+                model.addAttribute(ErrorMessage.getErrorName(result), ErrorMessage.getErrorValue(result));
+                return create(employee);
+            }
+
+        } catch (DataIntegrityViolationException e) {
+            model.addAttribute(ErrorMessage.getErrorName(ErrorKinds.DUPLICATE_EXCEPTION_ERROR),
+                    ErrorMessage.getErrorValue(ErrorKinds.DUPLICATE_EXCEPTION_ERROR));
+            return create(employee);
+        }
+
+        return "redirect:/employees";
+    }
+
+    // ■従業員更新画面
+    @GetMapping(value = "/{code}/update")
+    public String edit(@PathVariable String code, Model model) {
+
+        model.addAttribute("employee", employeeService.findByCode(code));
+        return "employees/update";
+    }
+
+    //　■エラー時更新画面再表示用
+    public String edit(@PathVariable ("code")String code,Employee employee, BindingResult res, Model model1) {
+        model1.addAttribute("employee", employee);
+        return "employees/update";
+    }
+
+    // ■従業員更新処理
+    @PostMapping(value = "/{code}/update")
+    public String update(@PathVariable String code, Model model,@Validated Employee employee2, BindingResult res, Model model1) {
+        // !!　voidをStringに直すこと
+        //System.out.println(code);//codeが画面から渡されているか確認用（クラスをvoidにすること）
+
+        // 入力チェック
+        if (res.hasErrors()) {
+
+            model1.addAttribute("employee", employeeService.findByCode(code));
+            return edit(code,employee2, res, model1);
+            //System.out.println("入力チェック時");//エラー内容確認用（クラスをvoidにすること）
+        }
+
+        // パスワード空白チェック
+        //　パスワードが空白の場合
+        /*if ("".equals(employee.getPassword())) {
+            Employee dbemployee=employeeService.findByCode(code);
+            String dbpassword=dbemployee.getPassword();
+            employee.setPassword(dbpassword);
+        }*/
+
+
+        // 論理削除を行った従業員番号を指定すると例外となるためtry~catchで対応
+        // (findByIdでは削除フラグがTRUEのデータが取得出来ないため)
+        try {
+            ErrorKinds result = employeeService.save(code,employee2);
+
+            if (ErrorMessage.contains(result)) {
+                model1.addAttribute(ErrorMessage.getErrorName(result), ErrorMessage.getErrorValue(result));
+                return edit(code,employee2, res, model1);
+                //System.out.println("サービスからの戻り値がエラー"+res);//エラー内容確認用（クラスをvoidにすること）
+            }
+
+        } catch (DataIntegrityViolationException e) {
+            model1.addAttribute(ErrorMessage.getErrorName(ErrorKinds.DUPLICATE_EXCEPTION_ERROR),
+                    ErrorMessage.getErrorValue(ErrorKinds.DUPLICATE_EXCEPTION_ERROR));
+            return edit(code,employee2, res, model1);
+            //System.out.println("削除フラグの例外発生");//エラー内容確認用（クラスをvoidにすること）
+        }
+
+
+
+        return "redirect:/employees";
+        //System.out.println("success");
+    }
+
+
+    // 従業員削除処理
+    @PostMapping(value = "/{code}/delete")
+    public String delete(@PathVariable String code, @AuthenticationPrincipal UserDetail userDetail, Model model) {
+
+        ErrorKinds result = employeeService.delete(code, userDetail);
+
+        if (ErrorMessage.contains(result)) {
+            model.addAttribute(ErrorMessage.getErrorName(result), ErrorMessage.getErrorValue(result));
+            model.addAttribute("employee", employeeService.findByCode(code));
+            return detail(code, model);
+        }
+
+        return "redirect:/employees";
+    }
+
+}
